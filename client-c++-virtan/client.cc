143ba8ba676ecd7c145954225a053db61f138522
Marat Abrarov <abrarov@gmail.com>
2019-02-23 22:55:12 +0300

    Replaced C rand with C++11 random device and distribution

--- client-c++-virtan/client.cc
+++ client-c++-virtan/client.cc
@@ -8,6 +8,7 @@
 #include <memory>
 #include <memory>
 #include <cstdlib>
+#include <random>
 #include <algorithm>
 #include <boost/asio.hpp>
 
@@ -39,12 +40,12 @@ public:
   }
 
   void start() {
-    start_time = std::chrono::steady_clock::now();
+    start_time_ = std::chrono::steady_clock::now();
   }
 
-  std::size_t current() {
+  std::size_t current() const {
     auto now = std::chrono::steady_clock::now();
-    return std::chrono::duration_cast<std::chrono::microseconds>(now - start_time).count();
+    return std::chrono::duration_cast<std::chrono::microseconds>(now - start_time_).count();
   }
 
   void reset() {
@@ -52,112 +53,115 @@ public:
   }
 
 private:
-  std::chrono::steady_clock::time_point start_time;
+  std::chrono::steady_clock::time_point start_time_;
 };
 
 class connection_handler : public std::enable_shared_from_this<connection_handler> {
 public:
-  connection_handler() :
-    service_id(++services_i),
-    my_service(*services[service_id % thread_num]),
-    s(my_service),
-    bear_tmr(my_service, boost::posix_time::microseconds(bear_after_mcs)),
-    exch_tmr(my_service),
-    errored(false) {}
+  explicit connection_handler(const std::mt19937& rand_engine) :
+    service_id_(++services_i),
+    my_service_(*services[service_id_ % thread_num]),
+    socket_(my_service_),
+    bear_timer_(my_service_, boost::posix_time::microseconds(bear_after_mcs)),
+    exch_timer_(my_service_),
+    errored_(false),
+    rand_engine_(rand_engine),
+    data_rand_(0, RAND_MAX),
+    exch_rand_(0, 2 * send_each_mcs) {}
 
   void start() {
-    if (service_id >= clients_max) {
+    if (service_id_ >= clients_max) {
       return;
     }
-    bear_tmr.async_wait(std::bind(
+    bear_timer_.async_wait(std::bind(
         &connection_handler::new_connection_handler, shared_from_this(), std::placeholders::_1));
-    connect_timer.reset();
+    connect_timer_.reset();
     ++connection_active;
-    boost::asio::async_connect(s, connect_to, std::bind(
+    boost::asio::async_connect(socket_, connect_to, std::bind(
         &connection_handler::connect, shared_from_this(), std::placeholders::_1));
   }
 
 private:
   void new_connection_handler(const boost::system::error_code& e) {
     assert(!e);
-    std::make_shared<connection_handler>()->start();
+    std::make_shared<connection_handler>(rand_engine_)->start();
   }
 
   void connect(const boost::system::error_code& e) {
     ++children;
     --connection_active;
     if (e) {
-      errored = true;
+      errored_ = true;
       ++connection_errors;
       return;
     }
-    std::size_t spent_mcs = connect_timer.current();
+    std::size_t spent_mcs = connect_timer_.current();
     if (spent_mcs > client_timeout_mcs) {
-      errored = true;
+      errored_ = true;
       ++connection_errors;
       return;
     }
     connection_summ_mcs += spent_mcs;
-    memset(send_buf, ' ', 32);
-    snprintf(send_buf, 32, "%d", rand());
-    send_buf[31] = '\n';
-    send_buf[32] = 0;
-    s.set_option(boost::asio::ip::tcp::no_delay(true));
+    memset(send_buf_, ' ', 32);
+    snprintf(send_buf_, 32, "%d", data_rand_(rand_engine_));
+    send_buf_[31] = '\n';
+    send_buf_[32] = 0;
+    socket_.set_option(boost::asio::ip::tcp::no_delay(true));
     read_staff(boost::system::error_code());
     send_staff(boost::system::error_code());
   }
 
   void send_staff(const boost::system::error_code& e) {
     assert(!e);
-    if (errored) {
+    if (errored_) {
       return;
     }
-    exch_tmr.expires_from_now(boost::posix_time::microseconds(rand() % (2 * send_each_mcs)));
-    exch_tmr.async_wait(std::bind(
+    exch_timer_.expires_from_now(boost::posix_time::microseconds(exch_rand_(rand_engine_) % (2 * send_each_mcs)));
+    exch_timer_.async_wait(std::bind(
         &connection_handler::send_staff, shared_from_this(), std::placeholders::_1));
-    tqueue.push(timer());
-    boost::asio::async_write(s, boost::asio::mutable_buffers_1(send_buf, 32), std::bind(
+    tqueue_.push(timer());
+    boost::asio::async_write(socket_, boost::asio::mutable_buffers_1(send_buf_, 32), std::bind(
         &connection_handler::stub, shared_from_this(), std::placeholders::_1));
   }
 
   void stub(const boost::system::error_code& e) {
-    if (errored) {
+    if (errored_) {
       return;
     }
     if (e) {
-      errored = true;
+      errored_ = true;
       ++exchange_errors;
       return;
     }
   }
 
   void read_staff(const boost::system::error_code& e) {
-    if (errored) {
+    if (errored_) {
       return;
     }
     if (e) {
-      errored = true;
+      errored_ = true;
       ++exchange_errors;
       return;
     }
-    memset(read_buf, 0, 33);
-    boost::asio::async_read(s, boost::asio::mutable_buffers_1(read_buf, 32), std::bind(
+    memset(read_buf_, 0, 33);
+    boost::asio::async_read(socket_, boost::asio::mutable_buffers_1(read_buf_, 32), std::bind(
         &connection_handler::compare_staff, shared_from_this(), std::placeholders::_1, std::placeholders::_2));
   }
 
   void compare_staff(const boost::system::error_code& e, std::size_t transferred) {
-    if (errored) {
+    if (errored_) {
       return;
     }
-    if (e || transferred != 32 || strncmp(send_buf, read_buf, 32)) {
-      errored = true;
+    if (e || transferred != 32 || strncmp(send_buf_, read_buf_, 32)) {
+      errored_ = true;
       ++exchange_errors;
       return;
     }
-    std::size_t spent_mcs = tqueue.front().current();
-    tqueue.pop();
+    std::size_t spent_mcs = tqueue_.front().current();
+    tqueue_.pop();
     if (spent_mcs > client_timeout_mcs) {
-      errored = true;
+      errored_ = true;
       ++exchange_errors;
       return;
     }
@@ -166,24 +170,27 @@ private:
     read_staff(e);
   }
 
-  std::size_t service_id;
-  boost::asio::io_service& my_service;
-  boost::asio::ip::tcp::socket s;
-  boost::asio::deadline_timer bear_tmr;
-  boost::asio::deadline_timer exch_tmr;
-  char send_buf[33];
-  char read_buf[33];
-  timer connect_timer;
-  bool errored;
-  std::queue<timer> tqueue;
+  std::size_t service_id_;
+  boost::asio::io_service& my_service_;
+  boost::asio::ip::tcp::socket socket_;
+  boost::asio::deadline_timer bear_timer_;
+  boost::asio::deadline_timer exch_timer_;
+  char send_buf_[33];
+  char read_buf_[33];
+  timer connect_timer_;
+  bool errored_;
+  std::queue<timer> tqueue_;
+  std::mt19937 rand_engine_;
+  std::uniform_int_distribution<int> data_rand_;
+  std::uniform_int_distribution<int> exch_rand_;
 };
 
 void print_stat(bool final = false) {
   if (final) {
     std::cout << "\nFinal statistics:\n";
   }
-  std::size_t conn_avg = connection_summ_mcs / std::max((std::size_t) children - connection_errors, (std::size_t) 1);
-  std::size_t lat_avg = latency_summ_mcs / std::max((std::size_t) msgs, (std::size_t) 1);
+  std::size_t conn_avg = connection_summ_mcs / (std::max)((std::size_t) children - connection_errors, (std::size_t) 1);
+  std::size_t lat_avg = latency_summ_mcs / (std::max)((std::size_t) msgs, (std::size_t) 1);
   std::cout << "Chld: " << children << " ConnErr: " << (final ? connection_errors + connection_active : static_cast<std::size_t>(connection_errors)) << " ExchErr: " << exchange_errors << " ConnAvg: " << (((double) conn_avg) / 1000) << "ms LatAvg: " << (((double) lat_avg) / 1000) << "ms  Msgs: " << msgs << "\n";
 }
 
@@ -197,6 +204,7 @@ int main(int args, char** argv) {
   std::string host = argv[1];
   std::string port = args > 2 ? argv[2] : "32000";
   thread_num = args > 3 ? std::atoi(argv[3]) : 24;
+  std::mt19937 rand_engine;
   std::vector<std::thread> threads;
   threads.reserve(thread_num);
   services.reserve(thread_num);
@@ -212,7 +220,7 @@ int main(int args, char** argv) {
   boost::asio::ip::tcp::resolver resolver(*services[0]);
   boost::asio::ip::tcp::resolver::query query(host, port);
   connect_to = resolver.resolve(query);
-  std::make_shared<connection_handler>()->start();
+  std::make_shared<connection_handler>(rand_engine)->start();
   for (std::size_t i = 0; i < 60; i += 5) {
     print_stat();
     std::this_thread::sleep_for(std::chrono::seconds(5));
